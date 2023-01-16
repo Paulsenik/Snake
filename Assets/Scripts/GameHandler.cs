57bb42506e7f8094f4cf18c4517dbf253c557619
Paul Seidel <seidel@fim.uni-passau.de>
2023-01-16 14:23:18 +0100

    Initial commit

---
+++ Assets/Scripts/GameHandler.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHandler : MonoBehaviour {
+
+    public static GameHandler gm;
+
+    public int width = 20, height = 20;
+    public Transform Snake;
+    public Transform Apple;
+    public Transform snakeBody;
+
+    public static Transform currentSnake, currentApple;
+
+    public List<Transform> bodyParts = new List<Transform>();
+    private bool isBodyToAdd = false;
+
+    void Start() {
+        gm = this;
+        Debug.Log("start");
+
+        currentApple = Instantiate(Apple);
+        randomizeApple();
+        currentSnake = Instantiate(Snake);
+    }
+
+    public void randomizeApple() {
+        currentApple.position = new Vector3(Random.Range(0, width), Random.Range(0, height));
+    }
+
+    private void Update() {
+        if (isBodyToAdd) {
+            isBodyToAdd = false;
+            Transform pos = transform;
+            if (bodyParts.Count > 0) {
+                pos = ((Transform)bodyParts[bodyParts.Count - 1]);
+            }
+            bodyParts.Add(Instantiate(snakeBody));
+        }
+    }
+
+    public void updateBodyParts() {
+        //Debug.Log("updateBody");
+        if(bodyParts.Count > 0) {
+            for(int i = bodyParts.Count-1; i >= 0; i--) {
+                if (i != 0) {
+                    bodyParts[i].position = bodyParts[i - 1].position;
+                } else {
+                    bodyParts[0].position = currentSnake.position;
+                }
+            }
+        }
+    }
+
+    public void addBodypart() {
+        isBodyToAdd = true;
+    }
+
+}
---
+++ Assets/Scripts/Snake.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Snake : MonoBehaviour
+{
+
+
+    public int startX = 10;
+    public int startY = 10;
+
+    private float countDown = 0f;
+    public float maxWaitTime = 1f;
+
+    private int speedX = 0, speedY = 1;
+    private int lastXSpeed = 0, lastYSpeed = 1;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(startX, startY);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x == GameHandler.currentApple.position.x && transform.position.y == GameHandler.currentApple.position.y) {
+            Debug.Log("eat");
+            GameHandler.gm.randomizeApple();
+            GameHandler.gm.addBodypart();
+        }
+        if ((Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) && lastYSpeed != -1) {
+            speedX = 0;
+            speedY = 1;
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+        if ((Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S)) && lastYSpeed != 1) {
+            speedX = 0;
+            speedY = -1;
+            transform.rotation = Quaternion.Euler(0, 0, 180);
+        }
+        if ((Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D)) && lastXSpeed != -1) {
+            speedX = 1;
+            speedY = 0;
+            transform.rotation = Quaternion.Euler(0, 0, -90);
+        }
+        if ((Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A)) && lastXSpeed != 1) {
+            speedX = -1;
+            speedY = 0;
+            transform.rotation = Quaternion.Euler(0, 0, 90);
+        }
+
+        countDown -= Time.deltaTime;
+        if(countDown <= 0) {
+            countDown = maxWaitTime;
+            move();
+            lastXSpeed = speedX;
+            lastYSpeed = speedY;
+
+            // out of Bounds
+            if (transform.position.x >= GameHandler.gm.width || transform.position.y >= GameHandler.gm.height || transform.position.x < 0 || transform.position.y < 0) {
+                Debug.Log("Snake hit Wall");
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+
+            // Snake hits itself
+            for (int i=0;i<GameHandler.gm.bodyParts.Count; i++) {
+                if (GameHandler.gm.bodyParts[i].position.Equals(transform.position)) {
+                    Debug.Log("Snake hit itself");
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                }
+            }
+        }
+    }
+
+
+    private void move() {
+        GameHandler.gm.updateBodyParts();
+        transform.position += new Vector3(speedX,speedY);
+    }
+}
